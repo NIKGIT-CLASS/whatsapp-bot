fbf484332f2517128aa865513888caebbe72a70d
NIKGIT-CLASS <fourcandel5@gmail.com>
2026-02-26 12:37:57 +0000

    Add WhatsApp message handling endpoint

--- server.js
+++ server.js
@@ -10,3 +10,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+app.post("/whatsapp", (req, res) => {
+  const reply = `
+    <Response>
+      <Message>👋 I got your WhatsApp message!</Message>
+    </Response>
+  `;
+  res.type("text/xml").send(reply);
+});
